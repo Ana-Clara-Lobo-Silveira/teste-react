e96a56c065b4b3fe47a592a85ee4917742784108
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-06-12 08:20:11 -0300

    ADD: Funções nos botões  de reiniciar, aumentar e diminuir.

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -2,21 +2,25 @@ import { useState } from "react"
 import styles from "./Contador.module.css"
 
 function Contador(){
-    // const [contador, setContador] = useState(0)
-    // atualizarContadorP = () =>{
-    //     setContador(contador + 1)
-    // }
-    // atualizarContadorN = () =>{
-    //     setContador(contador - 1)
-    // }
+    const [contador, setContador] = useState(0)
+    const atualizarContadorP = () =>{
+        setContador(contador + 1);
+    }
+    const atualizarContadorN = () =>{
+        setContador(contador - 1);
+    }
+    const reiniciarContador = () =>{
+        setContador(0);
+    }
+
     return(
         <>
         <section className={styles.body}>
-            <p className={styles.p}>00</p>
+            <p className={styles.p}>{contador}</p>
             <div className={styles.buttons}>
-                <button className={styles.button}>Reiniciar</button>            
-                <button className={styles.button}>+</button>
-                <button className={styles.button}>-</button>
+                <button className={styles.button} onClick={reiniciarContador}>Reiniciar</button>            
+                <button className={styles.button} onClick={atualizarContadorP}>+</button>
+                <button className={styles.button} onClick={atualizarContadorN}>-</button>
                 <button className={styles.button}>Sortear</button>
             </div>
 
