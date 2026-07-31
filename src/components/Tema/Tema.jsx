25d0a72ed044ffd2d91fdc2d09547c3cf50f3cfa
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-07-31 08:23:46 -0300

    ADD: Fuç~es do arquivo Tema.

--- src/components/Tema/Tema.jsx
+++ src/components/Tema/Tema.jsx
@@ -2,28 +2,39 @@ import { useEffect, useState } from "react";
 import styles from "./Tema.module.css";
 
 function Tema(){
-    // Controle de situações variáveis
+ // Controle de situações variáveis
     const [contador, setContador] = useState(0);
     const [nome, setNome] = useState("");
     const [temaEscuro, setTemaEscuro] = useState(false);
 // ---------------------------------------------------------------
-    
+
 // Executa apenas uma vez
-    useEffect(()=>{},[]);
-
+    useEffect(()=>{
+        console.log('Componente carregado pela primeira vez.')
+    },[]);
+    
     // Executa de acordo com a respectiva mudança
-    useEffect(()=>{},[contador]);
-    useEffect(()=>{},[temaEscuro]);
+    useEffect(()=>{
+        console.log(`O contador foi alterado para: ${contador}`)
+    },[contador]);
+    useEffect(()=>{
+        console.log('Tema alterado:', temaEscuro ? 'Escuro' : 'Claro')
+    },[temaEscuro]);
 // ---------------------------------------------------------------
-
-    // Funções de alternância
-    function aumentarContador(){};
-    function diminuirContador(){};
-    function alternarTema(){};
+    
+// Funções de alternância
+    function aumentarContador(){setContador(contador + 1);};
+    function diminuirContador(){setContador(contador - 1);};
+    function alternarTema(){setTemaEscuro(!temaEscuro)};
 // ---------------------------------------------------------------
 
     return(
-        <main></main>
+        <main className={`${styles.container} ${temaEscuro ? styles.temaEscuro : styles.temaClaro}`}>
+            
+            <section></section>
+            <section></section>
+            <section></section>
+        </main>
     );
 }
 export default Tema;
