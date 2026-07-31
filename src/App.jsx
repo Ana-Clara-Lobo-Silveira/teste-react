911b5a1cc6240b5f367226e12ca1982a8adb1980
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-07-31 09:49:29 -0300

    MODIFY: Correção de importações que estavam alterando o CSS da página Tema.

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Button from "./components/Button/Button";
 import Card from "./components/Card/Card";
 import MsgUsuario from "./components/MensagemUsuario/MsgUsuario";
 import UseStateHook from "./components/UseStateHook/UseStateHook";
-import Contador from "./components/Contador/Contador";
-import UseEffectHook from "./components/UseEffectHook/UseEffectHook";
+// import Contador from "./components/Contador/Contador";
+// import UseEffectHook from "./components/UseEffectHook/UseEffectHook";
 import Tema from "./components/Tema/Tema";
 
 function App() {
