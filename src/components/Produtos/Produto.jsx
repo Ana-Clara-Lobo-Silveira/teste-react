cef7ada0320c1a5d883082751b29fe35bc9da150
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-08-20 17:05:23 -0300

    MOD: Tratamento de erro caso a consulta na API falha.

--- src/components/Produtos/Produto.jsx
+++ src/components/Produtos/Produto.jsx
@@ -5,16 +5,19 @@ const Produto = ({produto}) =>{
     const [dados, setDados] = useState(null);
     useEffect(()=>{
         async function fetchProduto(){
+            if (!produto) return;
         const respFetch =  await fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`);
-        const dados = await respFetch.json();
-        setDados(dados);
-        console.log(dados);
+        const listaP = await respFetch.json();
+        setDados(listaP);
+        console.log(listaP);
     }
     fetchProduto();
     }, 
-    [])
+    [produto])
 
-
+if (dados === null){
+    return <p>Carregando...</p>
+}
 
     return(
         <>
